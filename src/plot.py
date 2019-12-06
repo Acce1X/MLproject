3b97a92f3f32d694499d51865f5186f18cdfa084
Acce1X <605000993@qq.com>
2019-12-06 20:42:30 +0800

    add model

--- src/plot.py
+++ src/plot.py
@@ -10,17 +10,17 @@ import pandas as pd
 
 
 df = pd.read_csv('./dataset.csv',header = 0)
-#df = pd.read_csv('./dataset_idf.csv',header = 0)#准确率更低了。。。
+#df = pd.read_csv('./dataset_idf.csv',header = 0)
 data = df.values
 X = data[:,1:100]
 y = data[:,-1].astype(int)
 cValue = ['r','y','g','b','r','y','g','b','r']  
 
 '''n_components维度降为2维,init设置embedding的初始化方式，可选random或者pca'''
-X_tsne = TSNE(n_components=2, init='random', random_state=0).fit_transform(X)
-X_pca = PCA().fit_transform(X)
+#X_tsne = TSNE(n_components=2, init='random', random_state=0).fit_transform(X)
+#X_pca = PCA().fit_transform(X)
 X_lda = discriminant_analysis.LinearDiscriminantAnalysis(n_components=2).fit_transform(X, y)
-X_iso = manifold.Isomap( n_components=2).fit_transform(X)
+#X_iso = manifold.Isomap( n_components=2).fit_transform(X)
 
 def plot_scatter(X,y,label,sub_no):
     plt.subplot(sub_no)
@@ -34,8 +34,9 @@ def plot_scatter(X,y,label,sub_no):
     plt.legend(tag2ill)
 
 
-plot_scatter(X_tsne,y,'X_tsne',221)
-plot_scatter(X_pca,y,'X_pca',222)
-plot_scatter(X_lda,y,'X_lda',223)
-plot_scatter(X_iso,y,'X_iso',224)
+#plot_scatter(X_tsne,y,'X_tsne',221)
+plot_scatter(X_lda,y,'X_pca',121)
+
+#plot_scatter(X_lda,y,'X_lda',223)
+#plot_scatter(X_iso,y,'X_iso',224)
 plt.show()
